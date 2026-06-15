f743ac311e7a0919267c2cf7135bd846650b9a9a
qkrwns1478 <qkrwns1478@gmail.com>
2026-06-16 00:27:16 +0900

    chore: gateway server 설정 추가

---
+++ user-service/src/main/java/com/bankrupang/sanjijk/user/UserTestController.java
@@ -0,0 +1,14 @@
+package com.bankrupang.sanjijk.user;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserTestController {
+
+    @GetMapping("/api/users/hello")
+    public String hello(){
+        return "Hello World";
+    }
+
+}
