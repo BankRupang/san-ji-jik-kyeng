3ed857e5db70d2b0dba67deba17069eb9f2be18d
dddd2356 <dddd2356@naver.com>
2026-06-17 03:37:34 +0900

    fix: JPA 미설정 서비스에서 EnableJpaAuditing 조건부 적용

--- common/src/main/java/com/bankrupang/sanjijk/common/CommonConfiguration.java
+++ common/src/main/java/com/bankrupang/sanjijk/common/CommonConfiguration.java
@@ -1,9 +1,11 @@
 package com.bankrupang.sanjijk.common;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @Configuration
+@ConditionalOnBean(name = "entityManagerFactory")
 @EnableJpaAuditing(auditorAwareRef = "auditorAwareImpl")
 public class CommonConfiguration {
 }
