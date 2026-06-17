0db332c4d3c594cdc7c254b9de83244f73ee0f3f
sinyeowon <ioohyou@knu.ac.kr>
2026-06-17 14:31:29 +0900

    feat: Product repository 추가

---
+++ services/auction-service/src/main/java/com/bankrupang/sanjijk/auction/product/domain/repository/ProductRepository.java
@@ -0,0 +1,20 @@
+package com.bankrupang.sanjijk.auction.product.domain.repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.bankrupang.sanjijk.auction.product.domain.entity.Product;
+
+public interface ProductRepository extends JpaRepository<Product, UUID> {
+
+    boolean existsByIdAndDeletedAtIsNull(UUID id);
+
+    Optional<Product> findByIdAndDeletedAtIsNull(UUID id);
+
+    boolean existsByIdAndSellerIdAndDeletedAtIsNull(UUID id, UUID sellerId);
+
+    Optional<Product> findByIdAndSellerIdAndDeletedAtIsNull(UUID id, UUID sellerId);
+
+}
