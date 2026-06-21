bfc90b31619d6bb3d57bbdafc62249f9f92b7153
sinyeowon <ioohyou@knu.ac.kr>
2026-06-21 23:33:16 +0900

    fix: auction-service JPA Auditing 활성화

--- services/auction-service/src/main/java/com/bankrupang/sanjijk/auction/AuctionServiceApplication.java
+++ services/auction-service/src/main/java/com/bankrupang/sanjijk/auction/AuctionServiceApplication.java
@@ -2,9 +2,11 @@ package com.bankrupang.sanjijk.auction;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditorAwareImpl")
 @EnableScheduling
 public class AuctionServiceApplication {
 
