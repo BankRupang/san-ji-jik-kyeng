92ae90c2b867cd1390faac04947150614fd1e57a
jimin Kim <gjimin26@gmail.com>
2026-06-23 15:15:39 +0900

    refact: 보증금 주문 생성 시 Feign 조회로 교체

--- services/order-service/src/main/java/com/bankrupang/sanjijk/order/presentation/dto/request/OrderDepositCreateRequest.java
+++ services/order-service/src/main/java/com/bankrupang/sanjijk/order/presentation/dto/request/OrderDepositCreateRequest.java
@@ -1,30 +1,11 @@
 package com.bankrupang.sanjijk.order.presentation.dto.request;
 
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
 
-import java.time.LocalDateTime;
 import java.util.UUID;
 
 public record OrderDepositCreateRequest(
         @NotNull(message = "경매 방 아이디는 필수 입니다.")
-        UUID auctionId,
-
-        @NotBlank(message = "경매 제목은 필수입니다.")
-        String auctionTitle,
-
-        @NotNull(message = "보증금 금액은 필수입니다.")
-        @Positive(message = "보증금 금액은 0보다 커야합니다.")
-        int amount,
-
-        @NotBlank(message = "유저 이름은 필수입니다.")
-        String userName,
-
-        @NotBlank(message = "슬랙 아이디는 필수입니다.")
-        String slackId,
-
-        @NotNull(message = "경매 종료 시각은 필수입니다.")
-        LocalDateTime endAt
+        UUID auctionId
 ) {
 }
